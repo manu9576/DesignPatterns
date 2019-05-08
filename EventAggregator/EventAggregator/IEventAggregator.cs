64d65eb16e96c72cb2ced4e95fdd61ba26879103
Emmanuel Lemaistre <manu9576@free.fr>
2019-05-08 12:52:55 +0200

    Cleanup

--- EventAggregator/EventAggregator/IEventAggregator.cs
+++ EventAggregator/EventAggregator/IEventAggregator.cs
@@ -1,4 +1,4 @@
-﻿ ﻿/*
+﻿/*
 * Gautham Prabhu K 2014
 * Copyright (c) 2014  gautham.prabhu.se@gmail.com
 * All rights reserved.
@@ -7,9 +7,6 @@
 */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace EventAggregator
 {
@@ -19,6 +16,4 @@ namespace EventAggregator
 
         void SubsribeEvent(Object subscriber);
     }
-
-   
 }
