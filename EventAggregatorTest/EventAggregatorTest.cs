cae37f2a4d9175397d32dff5183ec2f352fcd8aa
Emmanuel Lemaistre <manu9576@hotmail.fr>
2019-05-25 18:17:00 +0200

    Creation of test

---
+++ EventAggregatorTest/EventAggregatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EventAggregator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventAggregatorTest
+{
+    [TestClass]
+    public class EventAggregatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string response =string.Empty;
+
+            EventAggregator.EventAggregator.GetInstance<StringEvent>().Subscribe((str) => response = str.ToString());
+
+            EventAggregator.EventAggregator.GetInstance<StringEvent>().Publish("Test");
+
+            Assert.AreEqual("Test", response);
+
+        }
+
+
+        private class StringEvent : TEventType<object> { }
+        
+    }
+}
---
+++ EventAggregatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EventAggregatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EventAggregatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e2f795d-945f-4a1d-91c1-64b489a3fec8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
