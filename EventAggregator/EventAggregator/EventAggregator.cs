f9fa15d787bff987c5a0b404d2820c5d53798687
Emmanuel Lemaistre <manu9576@hotmail.fr>
2019-05-25 18:16:48 +0200

    Modification to prims behavior

--- EventAggregator/EventAggregator/EventAggregator.cs
+++ EventAggregator/EventAggregator/EventAggregator.cs
@@ -1,133 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 
 namespace EventAggregator
 {
-    public class EventAggregator : IEventAggregator
+    public static class EventAggregator
     {
-        private static EventAggregator _instance;
+        private static Dictionary<Type,ITEventType<object>> _eventTypes = new Dictionary<Type, ITEventType<object>>();
 
-        private readonly Dictionary<Type, List<WeakReference>> _eventSubsribers;
-
-        private readonly object _lockSubscriberDictionary;
-
-        private EventAggregator()
+        public static T GetInstance<T>() where T : ITEventType<object> , new()
         {
-            _lockSubscriberDictionary = new object();
-            _eventSubsribers = new Dictionary<Type, List<WeakReference>>();
-        }
-
-        public static IEventAggregator Instance
-        {
-            get
+            if (!_eventTypes.ContainsKey(typeof(T)))
             {
-                if (_instance == null)
-                {
-                    _instance = new EventAggregator();
-                }
-                return _instance;
-            }
-        }
-
-        #region IEventAggregator Members
-
-        /// <summary>
-        /// Publish an event
-        /// </summary>
-        /// <typeparam name="TEventType"></typeparam>
-        /// <param name="eventToPublish"></param>
-        public void PublishEvent<TEventType>(TEventType eventToPublish)
-        {
-            Type subsriberType = typeof(ISubscriber<>).MakeGenericType(eventToPublish.GetType());
-
-            List<WeakReference> subscribers = GetSubscriberList(subsriberType);
-
-            List<WeakReference> subsribersToBeRemoved = new List<WeakReference>();
-
-            foreach (WeakReference weakSubsriber in subscribers)
-            {
-                if (weakSubsriber.IsAlive)
-                {
-                    ISubscriber<TEventType> subscriber = (ISubscriber<TEventType>)weakSubsriber.Target;
-
-                    InvokeSubscriberEvent<TEventType>(eventToPublish, subscriber);
-                }
-                else
-                {
-                    subsribersToBeRemoved.Add(weakSubsriber);
-                }
+                _eventTypes.Add(typeof(T), new T());
             }
 
-            if (subsribersToBeRemoved.Any())
-            {
-                lock (_lockSubscriberDictionary)
-                {
-                    foreach (WeakReference remove in subsribersToBeRemoved)
-                    {
-                        subscribers.Remove(remove);
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Subribe to an event.
-        /// </summary>
-        /// <param name="subscriber"></param>
-        public void SubsribeEvent(object subscriber)
-        {
-            lock (_lockSubscriberDictionary)
-            {
-                IEnumerable<Type> subsriberTypes = subscriber.GetType().GetInterfaces()
-                                        .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ISubscriber<>));
-
-                WeakReference weakReference = new WeakReference(subscriber);
-
-                foreach (Type subsriberType in subsriberTypes)
-                {
-                    List<WeakReference> subscribers = GetSubscriberList(subsriberType);
-
-                    subscribers.Add(weakReference);
-                }
-            }
-        }
-
-        #endregion IEventAggregator Members
-
-        private void InvokeSubscriberEvent<TEventType>(TEventType eventToPublish, ISubscriber<TEventType> subscriber)
-        {
-            //Synchronize the invocation of method
-
-            SynchronizationContext syncContext = SynchronizationContext.Current;
-
-            if (syncContext == null)
-            {
-                syncContext = new SynchronizationContext();
-            }
-
-            syncContext.Post(s => subscriber.OnEventHandler(eventToPublish), null);
-        }
-
-        private List<WeakReference> GetSubscriberList(Type subsriberType)
-        {
-            List<WeakReference> subsribersList = null;
-
-            lock (_lockSubscriberDictionary)
-            {
-                bool found = this._eventSubsribers.TryGetValue(subsriberType, out subsribersList);
-
-                if (!found)
-                {
-                    //First time create the list.
-
-                    subsribersList = new List<WeakReference>();
-
-                    this._eventSubsribers.Add(subsriberType, subsribersList);
-                }
-            }
-            return subsribersList;
+            return (T)_eventTypes[typeof(T)] ;
         }
     }
 }
---
+++ EventAggregator/EventAggregator/TEventType.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventAggregator
+{
+    public class TEventType<T> : ITEventType<T>
+    {
+
+        private readonly List<WeakReference> _eventSubsribers;
+
+        private readonly object _lockSubscriberDictionary;
+
+        public TEventType()
+        {
+            _lockSubscriberDictionary = new object();
+            _eventSubsribers = new List<WeakReference>();
+        }
+
+        //#region IEventAggregator Members
+
+        /// <summary>
+        /// Publish an event
+        /// </summary>
+        /// <typeparam name="TEventType"></typeparam>
+        /// <param name="eventToPublish"></param>
+        //public void PublishEvent<TEventType>(TEventType eventToPublish)
+        //{
+        //    Type subsriberType = typeof(ISubscriber<>).MakeGenericType(eventToPublish.GetType());
+
+        //    List<WeakReference> subscribers = GetSubscriberList(subsriberType);
+
+        //    List<WeakReference> subsribersToBeRemoved = new List<WeakReference>();
+
+        //    foreach (WeakReference weakSubsriber in subscribers)
+        //    {
+        //        if (weakSubsriber.IsAlive)
+        //        {
+        //            ISubscriber<TEventType> subscriber = (ISubscriber<TEventType>)weakSubsriber.Target;
+
+        //            InvokeSubscriberEvent<TEventType>(eventToPublish, subscriber);
+        //        }
+        //        else
+        //        {
+        //            subsribersToBeRemoved.Add(weakSubsriber);
+        //        }
+        //    }
+
+        //    if (subsribersToBeRemoved.Any())
+        //    {
+        //        lock (_lockSubscriberDictionary)
+        //        {
+        //            foreach (WeakReference remove in subsribersToBeRemoved)
+        //            {
+        //                subscribers.Remove(remove);
+        //            }
+        //        }
+        //    }
+        //}
+
+        /// <summary>
+        /// Subribe to an event.
+        /// </summary>
+        /// <param name="subscriber"></param>
+        //public void SubsribeEvent(object subscriber)
+        //{
+        //    lock (_lockSubscriberDictionary)
+        //    {
+        //        IEnumerable<Type> subsriberTypes = subscriber.GetType().GetInterfaces()
+        //                                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ISubscriber<>));
+
+        //        WeakReference weakReference = new WeakReference(subscriber);
+
+        //        foreach (Type subsriberType in subsriberTypes)
+        //        {
+        //            List<WeakReference> subscribers = GetSubscriberList(subsriberType);
+
+        //            subscribers.Add(weakReference);
+        //        }
+        //    }
+        //}
+
+        //#endregion IEventAggregator Members
+
+        //private void InvokeSubscriberEvent<TEventType>(TEventType eventToPublish, ISubscriber<TEventType> subscriber)
+        //{
+        //    Synchronize the invocation of method
+
+        //    SynchronizationContext syncContext = SynchronizationContext.Current;
+
+        //    if (syncContext == null)
+        //    {
+        //        syncContext = new SynchronizationContext();
+        //    }
+
+        //    syncContext.Post(s => subscriber.OnEventHandler(eventToPublish), null);
+        //}
+
+        //private List<WeakReference> GetSubscriberList(Type subsriberType)
+        //{
+        //    List<WeakReference> subsribersList = null;
+
+        //    lock (_lockSubscriberDictionary)
+        //    {
+        //        bool found = this._eventSubsribers.TryGetValue(subsriberType, out subsribersList);
+
+        //        if (!found)
+        //        {
+        //            First time create the list.
+
+        //            subsribersList = new List<WeakReference>();
+
+        //            this._eventSubsribers.Add(subsriberType, subsribersList);
+        //        }
+        //    }
+        //    return subsribersList;
+        //}
+
+
+        public void Publish(T eventToPublish)
+        {
+            foreach(var sub in _eventSubsribers)
+            {
+                if(sub.IsAlive)
+                {
+                    var action = sub.Target as Action<T>;
+
+                    action.Invoke(eventToPublish);
+                }
+            }
+        }
+
+        public void Subscribe(Action<T> action)
+        {
+            var weakReference  = new WeakReference(action);
+
+            _eventSubsribers.Add(weakReference);
+
+
+        }
+
+        public void Unsubscribe(Action<T> action)
+        {
+
+        }
+    }
+}
