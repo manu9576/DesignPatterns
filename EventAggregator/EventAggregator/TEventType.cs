37d73e6ad2651cfb32d02c76bdc1d0f20fe88720
Emmanuel Lemaistre <manu9576@hotmail.fr>
2019-06-11 22:34:44 +0200

    Adding ThreadOptions at subscription

--- EventAggregator/EventAggregator/TEventType.cs
+++ EventAggregator/EventAggregator/TEventType.cs
@@ -1,47 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Windows;
 
 namespace EventAggregator
 {
     public class TEventType<T> : EventBase
     {
-        private readonly List<WeakReference> _eventSubsribers;
+        private readonly List<Tuple<WeakReference, ThreadOptions>> _eventSubsribers;
 
         private readonly object _lockSubscriberDictionary;
 
         public TEventType()
         {
             _lockSubscriberDictionary = new object();
-            _eventSubsribers = new List<WeakReference>();
+            _eventSubsribers = new List<Tuple<WeakReference,ThreadOptions>>();
         }
 
         public void Publish(T eventToPublish)
         {
-            foreach (WeakReference sub in _eventSubsribers)
+            foreach (var sub in _eventSubsribers)
             {
-                if (sub.IsAlive)
+                if (sub.Item1.IsAlive)
                 {
-                    Action<T> action = sub.Target as Action<T>;
+                    Action<T> action = sub.Item1.Target as Action<T>;
 
-                    action.Invoke(eventToPublish);
+                    switch(sub.Item2)
+                    {
+                        case ThreadOptions.PublisherThread:
+                            action.Invoke(eventToPublish);
+                            break;
+
+                        case ThreadOptions.UIThread:
+                            Application.Current.Dispatcher.BeginInvoke(action, eventToPublish);
+                            break;
+
+                        case ThreadOptions.BackgroundThread:
+
+                            var bw = new BackgroundWorker();
+                            bw.DoWork += (_,__) =>
+                            {
+                                action.Invoke(eventToPublish);
+                            };
+                            bw.RunWorkerAsync();
+                            break;
+                    }
                 }
             }
         }
 
-        public void Subscribe(Action<T> action)
+        public void Subscribe(Action<T> action, ThreadOptions threadOption = ThreadOptions.PublisherThread)
         {
             WeakReference weakReference = new WeakReference(action);
 
-            if (_eventSubsribers.All(wr => (wr.Target as Action<T>) != action))
+            if (!_eventSubsribers.Any(wr => (wr.Item1.Target as Action<T>) == action))
             {
-                _eventSubsribers.Add(weakReference);
+                _eventSubsribers.Add(new Tuple<WeakReference,ThreadOptions>( weakReference, threadOption));
             }
         }
 
         public void Unsubscribe(Action<T> action)
         {
-            var weakReferenceToRemove = _eventSubsribers.FirstOrDefault(wr => wr.Target as Action<T> == action);
+            var weakReferenceToRemove = _eventSubsribers.FirstOrDefault(wr => wr.Item1.Target as Action<T> == action);
 
             if (weakReferenceToRemove != null)
             {
---
+++ EventAggregator/EventAggregator/ThreadOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventAggregator
+{
+    public enum ThreadOptions
+    {
+        PublisherThread,  // Use this setting to receive the event on the publishers' thread. This is the default setting.
+        BackgroundThread, // Use this setting to asynchronously receive the event on a .NET Framework thread-pool thread.
+        UIThread          // Use this setting to receive the event on UI's thead
+    }
+}
